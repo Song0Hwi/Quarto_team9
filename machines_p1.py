9af06b87e8faf0c3c468b8d3b5609715ef69a3a2
0Hwi Song <iamsongyounghwi@gamil.com>
2024-12-10 17:26:10 +0900

    다섯번째

--- machines_p1.py
+++ machines_p1.py
@@ -1,69 +1,80 @@
 import numpy as np
 from itertools import product
 import random
+import copy
 
-# 보드 크기 및 기타 상수 정의
+# 보드 크기 및 상수 정의
 BOARD_ROWS = 4
 BOARD_COLS = 4
 WIN_SCORE = 10
 CENTER_WEIGHT = 5
 THREAT_PENALTY = 10
+SIMULATION_COUNT = 10
+
 
 class P1:
     def __init__(self, board, available_pieces):
-        # 기본 게임 데이터 초기화
         self.pieces = [(i, j, k, l) for i in range(2) for j in range(2) for k in range(2) for l in range(2)]
         self.board = board
         self.available_pieces = available_pieces
-        self.center_positions = [(2, 2), (2, 3), (3, 2), (3, 3)]
+        self.center_positions = [(1, 1), (1, 2), (2, 1), (2, 2)]
 
-    # 상대방의 최적 조각 평가
     def evaluate_piece_for_opponent(self, piece):
-        score = 0
-        for row, col in self.get_available_locations():
-            simulated_board = self.simulate_board(row, col, piece)
-            if self.check_win(simulated_board):
-                score += WIN_SCORE
-            score -= self.evaluate_threat(simulated_board)
-        return score
+        lose_score = 0
+        gain_score = 0
 
-    # 상대방이 사용할 최적의 조각을 선택
+        for _ in range(SIMULATION_COUNT):
+            simulated_board = copy.deepcopy(self.board)
+            available_locations = copy.deepcopy(self.get_available_locations())
+            simulated_piece = copy.deepcopy(piece)
+
+            for row, col in available_locations:
+                simulated_board[row][col] = self.pieces.index(simulated_piece) + 1
+
+                if self.check_win(simulated_board):
+                    lose_score += WIN_SCORE
+
+                lose_score += self.evaluate_threat(simulated_board)
+
+                for my_row, my_col in self.get_available_locations():
+                    my_simulated_board = copy.deepcopy(simulated_board)
+                    my_simulated_board[my_row][my_col] = self.pieces.index(simulated_piece) + 1
+                    if self.check_win(my_simulated_board):
+                        gain_score -= WIN_SCORE
+
+        return lose_score - gain_score
+
     def select_piece(self):
         best_piece = None
         min_score = float('inf')
+
         for piece in self.available_pieces:
             score = self.evaluate_piece_for_opponent(piece)
             if score < min_score:
                 min_score = score
                 best_piece = piece
+
         return best_piece
 
-    # 위치 제어를 평가
     def evaluate_position_control(self, row, col, piece):
         score = 0
         simulated_board = self.simulate_board(row, col, piece)
 
-        # 중앙 위치에 가중치를 부여
         if (row, col) in self.center_positions:
             score += CENTER_WEIGHT
 
-        # 승리 여부 체크
         if self.check_win(simulated_board):
             score += WIN_SCORE
 
-        # 위협에 대한 페널티 적용
         score -= self.evaluate_threat(simulated_board)
         return score
 
-    # 선택한 조각을 보드에 놓기
     def place_piece(self, selected_piece):
-        # 첫 번째 수일 경우 중앙 위치를 우선 선택
         if self.is_first_move():
             for row, col in self.center_positions:
                 if self.board[row][col] == 0:
                     return row, col
 
-        # 알파베타 가지치기를 적용하여 가장 좋은 수를 선택
         best_positions = []
         max_score = -float('inf')
         alpha = -float('inf')
@@ -72,36 +83,29 @@ class P1:
         for row, col in self.get_available_locations():
             score = self.evaluate_position_control(row, col, selected_piece)
 
-            # 알파베타 가지치기 적용
             if score > max_score:
                 max_score = score
                 best_positions = [(row, col)]
             elif score == max_score:
                 best_positions.append((row, col))
 
-            # 알파베타 가지치기 로직
             alpha = max(alpha, score)
             if beta <= alpha:
-                break  # 더 이상 탐색할 필요 없음
+                break
 
-        # 동점일 경우 랜덤 선택
         return random.choice(best_positions)
 
-    # 첫 번째 수인지 확인하는 함수
     def is_first_move(self):
         return np.count_nonzero(self.board) == 0
 
-    # 사용 가능한 위치를 반환
     def get_available_locations(self):
         return [(row, col) for row, col in product(range(BOARD_ROWS), range(BOARD_COLS)) if self.board[row][col] == 0]
 
-    # 보드 상태에 선택된 조각을 놓은 후의 보드를 반환
     def simulate_board(self, row, col, piece):
         simulated_board = np.copy(self.board)
         simulated_board[row][col] = self.pieces.index(piece) + 1
         return simulated_board
 
-    # 승리 여부를 체크
     def check_win(self, board):
         def check_line(line):
             if 0 in line:
@@ -112,19 +116,20 @@ class P1:
                     return True
             return False
 
-        def check_2x2_win():
-            for row in range(BOARD_ROWS - 1):
-                for col in range(BOARD_COLS - 1):
-                    subgrid = [board[row][col], board[row][col + 1],
-                               board[row + 1][col], board[row + 1][col + 1]]
-                    if 0 not in subgrid:
-                        characteristics = np.array([self.pieces[piece_idx - 1] for piece_idx in subgrid])
-                        for i in range(4):
-                            if len(set(characteristics[:, i])) == 1:
+        def check_square():
+            for i in range(BOARD_ROWS - 1):
+                for j in range(BOARD_COLS - 1):
+                    square = [
+                        board[i][j], board[i][j + 1],
+                        board[i + 1][j], board[i + 1][j + 1]
+                    ]
+                    if 0 not in square:
+                        characteristics = np.array([self.pieces[piece_idx - 1] for piece_idx in square])
+                        for k in range(4):
+                            if len(set(characteristics[:, k])) == 1:
                                 return True
             return False
 
-        # 행, 열, 대각선, 2x2 체크
         for col in range(BOARD_COLS):
             if check_line([board[row][col] for row in range(BOARD_ROWS)]):
                 return True
@@ -133,25 +138,34 @@ class P1:
                 return True
         if check_line([board[i][i] for i in range(BOARD_ROWS)]) or check_line([board[i][BOARD_ROWS - i - 1] for i in range(BOARD_ROWS)]):
             return True
-        return check_2x2_win()
 
-    # 위협을 평가
+        return check_square()
+
     def evaluate_threat(self, board):
         def check_line_threat(line):
             filled_positions = [piece for piece in line if piece != 0]
-            if len(filled_positions) == 2:  # 2개의 조각이 있는 경우 위협 평가
-                characteristics = np.array([self.pieces[piece_idx - 1] for piece_idx in filled_positions])
-                for i in range(4):
-                    if len(set(characteristics[:, i])) == 1:  # 공통 특성이 있으면 위협으로 간주
-                        return THREAT_PENALTY
-            elif len(filled_positions) == 3:  # 3개의 조각이 있는 경우
+            if len(filled_positions) == 3:
                 characteristics = np.array([self.pieces[piece_idx - 1] for piece_idx in filled_positions])
                 for i in range(4):
                     if len(set(characteristics[:, i])) == 1:
                         return THREAT_PENALTY
             return 0
 
-        # 행, 열, 대각선, 2x2 위협 체크
+        def check_square_threat():
+            for i in range(BOARD_ROWS - 1):
+                for j in range(BOARD_COLS - 1):
+                    square = [
+                        board[i][j], board[i][j + 1],
+                        board[i + 1][j], board[i + 1][j + 1]
+                    ]
+                    filled_positions = [piece for piece in square if piece != 0]
+                    if len(filled_positions) == 3:
+                        characteristics = np.array([self.pieces[piece_idx - 1] for piece_idx in filled_positions])
+                        for k in range(4):
+                            if len(set(characteristics[:, k])) == 1:
+                                return THREAT_PENALTY
+            return 0
+
         for col in range(BOARD_COLS):
             if check_line_threat([board[row][col] for row in range(BOARD_ROWS)]):
                 return THREAT_PENALTY
@@ -161,15 +175,4 @@ class P1:
         if check_line_threat([board[i][i] for i in range(BOARD_ROWS)]) or check_line_threat([board[i][BOARD_ROWS - i - 1] for i in range(BOARD_ROWS)]):
             return THREAT_PENALTY
 
-        # 2x2 위협 체크
-        for row in range(BOARD_ROWS - 1):
-            for col in range(BOARD_COLS - 1):
-                subgrid = [board[row][col], board[row][col + 1],
-                           board[row + 1][col], board[row + 1][col + 1]]
-                if 0 in subgrid:
-                    continue
-                characteristics = np.array([self.pieces[piece_idx - 1] for piece_idx in subgrid])
-                for i in range(4):
-                    if len(set(characteristics[:, i])) == 1:
-                        return THREAT_PENALTY
-        return 0
+        return check_square_threat()
